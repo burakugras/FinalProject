2f712493e7da560d365bf308d0ae153b5117d146
PC <burakugras67@gmail.com>
2023-11-21 00:24:44 +0300

    GetAllByCategory operation is added

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -39,6 +39,11 @@ namespace DataAccess.Concrete.InMemory
             return _products;
         }
 
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p=>p.CategoryId==categoryId).ToList();
+        }
+
         public void Update(Product product)
         {
             Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
